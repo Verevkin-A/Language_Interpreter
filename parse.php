d435c0cea1650f4f15125bbfb2170aee8cdf4d66
Verevkin-A <sandyverevkin@gmail.com>
2022-02-28 16:34:04 +0100

    New: return codes + help argument

--- parse.php
+++ parse.php
@@ -1,9 +1,62 @@
 <?php
 ini_set('display_errors', 'stderr');
 
-while ($line = fgets(STDIN)) {
-    echo $line;
+include "parser_ext/Ret_Codes.php";
+
+const HELP = <<<EOD
+            The script of filter type reads the source code in IPP-code22, 
+            checks the lexical and syntactic correctness of the code and 
+            prints to standard output XML representation of the program. 
+            
+            Usage: php8.1 parse.php < file [--help]
+              --help prints short help message\n
+EOD;
+
+switch ($argc) {
+    case 1:
+        break;
+    case 2:
+        if ($argv[1] == "--help") {
+            echo HELP;
+            exit;
+        } else {
+            fwrite(STDERR, "Error: unknown parameter\n");
+            exit(Ret_Codes::PARAM_ERR);
+        }
+    default:
+        fwrite(STDERR, "Error: wrong count of parameters\n");
+        exit(Ret_Codes::PARAM_ERR);
 }
 
-//$line = trim(explode("#", $line)[0]);
-//if ($line == "") continue;
+$header = false;
+while ($line = fgets(STDIN)) {
+    $line = trim(explode("#", $line)[0]);
+    if ($line == "") {
+        continue;
+    }
+    // check input for .ippcode22 header
+    if (!$header) {
+        if(preg_match("%^\.IPPcode22$%i", $line)) {
+            $header = true;
+            continue;
+        } else {
+            fwrite(STDERR, "Input file should start with '.IPPcode22' header\n");
+            exit(Ret_Codes::HEADER_ERR);
+        }
+    }
+    $splitted = explode(" ", $line);
+    foreach ($splitted as $word) {
+        echo $word;
+    }
+
+//    echo $line;
+}
+
+
+
+
+
+
+
+
+exit;
---
+++ parser_ext/Ret_Codes.php
@@ -0,0 +1,14 @@
+<?php
+
+final class Ret_Codes
+{
+    const PARAM_ERR = 10;
+    const INPUT_ERR = 11;
+    const OUTPUT_ERR = 12;
+
+    const HEADER_ERR = 21;
+    const OPCODE_ERR = 22;
+    const LEX_SYN_ERR = 23;
+
+    const INTERNAL_ERR = 99;
+}
