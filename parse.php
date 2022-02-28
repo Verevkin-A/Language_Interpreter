4856fd84177f946c4322c13f06fe92f191e4c28f
Verevkin-A <sandyverevkin@gmail.com>
2022-02-28 22:43:32 +0100

    Feat: opcodes switch case

--- parse.php
+++ parse.php
@@ -28,6 +28,30 @@ switch ($argc) {
         exit(Ret_Codes::PARAM_ERR);
 }
 
+$xml = new DOMDocument("1.0", "UTF-8");
+$xml->formatOutput = true;
+
+$xml_program = $xml->createElement("program");
+$xml_program = $xml->appendChild($xml_program);
+
+$xml_program->setAttribute("language", "IPPcode22");
+
+//$xml = new DomDocument("1.0", "UTF-8");
+//$xml->formatOutput = true;	// Better formatting
+//
+//$xml_program = $xml->createElement("program");
+//$xml_program = $xml->appendChild($xml_program);
+//
+//$language_A = $xml->createAttribute("language");
+//$language_A->value = "IPPcode22";
+//$xml_program->appendChild($language_A);
+//
+//$xml_inst = $xml->createElement("instruction");
+//$xml_program->appendChild($xml_inst);
+//
+//echo $xml->saveXML();
+//exit(1);
+
 $header = false;
 while ($line = fgets(STDIN)) {
     $line = trim(explode("#", $line)[0]);
@@ -44,19 +68,98 @@ while ($line = fgets(STDIN)) {
             exit(Ret_Codes::HEADER_ERR);
         }
     }
-    $splitted = explode(" ", $line);
-    foreach ($splitted as $word) {
-        echo $word;
+    $line_args = explode(" ", $line);
+    switch ($line_args[0]) {
+        # no operands
+        case "CREATEFRAME":
+        case "PUSHFRAME":
+        case "POPFRAME":
+        case "RETURN":
+        case "BREAK":
+            if (count($line_args) != 1) {
+                fwrite(STDERR, "Error: wrong count of operands\n");
+                exit(Ret_Codes::LEX_SYN_ERR);
+            }
+            break;
+        # <var>
+        case "DEFVAR":
+        case "POPS":
+            if (count($line_args) != 2) {
+                fwrite(STDERR, "Error: wrong count of operands\n");
+                exit(Ret_Codes::LEX_SYN_ERR);
+            }
+            break;
+        # <label>
+        case "CALL":
+        case "LABEL":
+        case "JUMP":
+//            echo "<label>";
+            break;
+        # <symb>
+        case "PUSHS":
+        case "WRITE":
+        case "EXIT":
+        case "DPRINT":
+//            echo "<symb>";
+            break;
+        # <var> <symb>
+        case "MOVE":
+        case "INT2CHAR":
+        case "STRLEN":
+        case "TYPE":
+            if (count($line_args) != 3) {
+                fwrite(STDERR, "Error: wrong count of operands\n");
+                exit(Ret_Codes::LEX_SYN_ERR);
+            }
+            break;
+        # <var> <type>
+        case "READ":
+//            echo "<var> <type>";
+            break;
+        # <var> <symb1> <symb2>
+        case "ADD":
+        case "SUB":
+        case "MUL":
+        case "IDIV":
+        case "LT":
+        case "GT":
+        case "EQ":
+        case "AND":
+        case "OR":
+        case "NOT":
+        case "STRI2INT":
+        case "CONCAT":
+        case "GETCHAR":
+        case "SETCHAR":
+            if (count($line_args) != 4) {
+                fwrite(STDERR, "Error: wrong count of operands\n");
+                exit(Ret_Codes::LEX_SYN_ERR);
+            }
+            break;
+        # <label> <symb1> <symb2>
+        case "JUMPIFEQ":
+        case "JUMPIFNEQ":
+//            echo "<label> <symb1> <symb2>";
+            break;
+        default:
+            fwrite(STDERR, "Error: unknown opcode\n");
+            exit(Ret_Codes::OPCODE_ERR);
     }
 
-//    echo $line;
+
 }
 
+$xmlInstruction = $xml->createElement("instruction");
+$xmlInstruction->setAttribute("order", "lol");
+$xmlInstruction->setAttribute("opcode", "lol2");
 
+$xmlArg1 = $xml->createElement("arg1", "lolzs");
+$xmlArg1->setAttribute("type", "var");
+$xmlInstruction->appendChild($xmlArg1);
 
+$xml_program = $xml->appendChild($xmlInstruction);
 
-
-
+echo $xml->saveXML();
 
 
 exit;
